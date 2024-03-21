eec95b4017a6b8c7c356db20b7e44afb61b315f1
rlathddk <abc777z@naver.com>
2024-03-21 18:24:06 +0900

    2024-03-21-1823

--- src/main/java/ezenweb/model/entity/BoardEntity.java
+++ src/main/java/ezenweb/model/entity/BoardEntity.java
@@ -1,22 +1,43 @@
 package ezenweb.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity // 해당 클래스와 연동DB내 테이블과 매핑/연결 ( ORM )
+@Table(name = "board")
 @Setter
 @NoArgsConstructor@AllArgsConstructor
 @Builder
+
 public class BoardEntity { // 테이블
     @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int bno;        // 게시물번호 PK
+    @Column(name = "title", length = 10, nullable = false, unique = true)
     private String btitle; // 게시물제목
 
+    @Column(columnDefinition = "longtext")
+    private String btitle2;
+
+    @Column(columnDefinition = "date")
+    private String btitle3;
+
+    private boolean 필드0;
+
+    private byte 필드1;
+    private short 필드2;
+    private long 필드3;
+
+    private char 필드4;
+
+    private float 필드6;
+    private double 필드5;
+
+    private Date 필드7;
+    private LocalDateTime 필드8;
 }
 /*
     create table BoardEntitiy(
