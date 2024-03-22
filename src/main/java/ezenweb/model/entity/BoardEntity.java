4c92bb4c5f6d7100f0564df8454a7caec928b7f3
geonwk95 <abc777z@naver.com>
2024-03-22 18:06:00 +0900

    2024-03-22-1805

--- src/main/java/ezenweb/model/entity/BoardEntity.java
+++ src/main/java/ezenweb/model/entity/BoardEntity.java
@@ -16,28 +16,30 @@ public class BoardEntity { // 테이블
     @Id // PK
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int bno;        // 게시물번호 PK
-    @Column(name = "title", length = 10, nullable = false, unique = true)
     private String btitle; // 게시물제목
+    @JoinColumn // fk
+    @ManyToOne // 다수가 하나에게  M:1
+    private MemberEntity memberEntity;
 
-    @Column(columnDefinition = "longtext")
-    private String btitle2;
-
-    @Column(columnDefinition = "date")
-    private String btitle3;
-
-    private boolean 필드0;
-
-    private byte 필드1;
-    private short 필드2;
-    private long 필드3;
-
-    private char 필드4;
-
-    private float 필드6;
-    private double 필드5;
-
-    private Date 필드7;
-    private LocalDateTime 필드8;
+//    @Column(columnDefinition = "longtext")
+//    private String btitle2;
+//
+//    @Column(columnDefinition = "date")
+//    private String btitle3;
+//
+//    private boolean 필드0;
+//
+//    private byte 필드1;
+//    private short 필드2;
+//    private long 필드3;
+//
+//    private char 필드4;
+//
+//    private float 필드6;
+//    private double 필드5;
+//
+//    private Date 필드7;
+//    private LocalDateTime 필드8;
 }
 /*
     create table BoardEntitiy(
